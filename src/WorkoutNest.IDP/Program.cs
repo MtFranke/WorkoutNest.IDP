a3f2bf2bcf8534233bf38e1fea19e392fea2b8d5
MtFranke <mateusz.franke@proton.me>
2023-12-17 17:54:16 +0100

    base identity server by deunde impl.

--- src/WorkoutNest.IDP/Program.cs
+++ src/WorkoutNest.IDP/Program.cs
@@ -1,10 +1,22 @@
+using WorkoutNest.IDP;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddIdentityServer(options =>
+{
+    options.Events.RaiseErrorEvents = true;
+    options.Events.RaiseFailureEvents = true;
+    options.Events.RaiseInformationEvents = true;
+    options.Events.RaiseSuccessEvents = true;
+})
+    .AddInMemoryApiScopes(Config.ApiScopes)
+    .AddInMemoryClients(Config.Clients);;
 
 
 var app = builder.Build();
+
 app.UseHttpsRedirection();
 app.MapGet("/", () => "Hello World!");
-
-
+app.UseIdentityServer();
 app.Run();
 
