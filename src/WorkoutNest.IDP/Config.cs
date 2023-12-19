42ed3b07b72c60dbd4fb6300c55da9eeb503f81b
MtFranke <mateusz.franke@proton.me>
2023-12-19 18:03:05 +0100

    add user password flow

--- src/WorkoutNest.IDP/Config.cs
+++ src/WorkoutNest.IDP/Config.cs
@@ -1,33 +1,81 @@
+using System.Security.Claims;
 using Duende.IdentityServer.Models;
+using Duende.IdentityServer.Test;
+using IdentityModel;
 
 namespace WorkoutNest.IDP;
 
 public static class Config
 {
+
+    internal const string Id = "user_id";
+    internal const string UUID = "F185D000-BAD0-4BA8-AC53-88D6124B81F3";
+    
+    
+    public static List<TestUser> Users =>
+    [
+        new TestUser
+        {
+            SubjectId = UUID,
+            Username = "MtFranke",
+            Password = "asd",
+            Claims =
+            {
+                new Claim(JwtClaimTypes.Name, "Mateusz Franke"),
+                new Claim(JwtClaimTypes.GivenName, "Mateusz"),
+                new Claim(JwtClaimTypes.FamilyName, "Franke"),
+                new Claim(JwtClaimTypes.Email, "mateusz.franke@proton.me"),
+                new Claim(Id, UUID)
+
+            }
+        }
+    ];
+
+
     public static IEnumerable<Client> Clients =>
         new List<Client>
         {
             new Client
             {
-                ClientId = "client",
-
-                // no interactive user, use the clientid/secret for authentication
-                AllowedGrantTypes = GrantTypes.ClientCredentials,
-
+                ClientId = "workout-nest",
+                AllowedGrantTypes = GrantTypes.ResourceOwnerPasswordAndClientCredentials,
                 // secret for authentication
                 ClientSecrets =
                 {
                     new Secret("secret".Sha256())
                 },
-
                 // scopes that client has access to
-                AllowedScopes = { "api1" }
+                AllowedScopes = { "workout-nest.user", "openid", "profile" },
+                
             }
+        
         };
     
     public static IEnumerable<ApiScope> ApiScopes =>
         new List<ApiScope>
         {
-            new ApiScope(name: "api1", displayName: "MyAPI") 
+            new(name: "workout-nest.user") 
         };
+
+    public static IEnumerable<ApiResource> GetApiResources()
+    {
+        return new List<ApiResource>
+        {
+            new ApiResource
+            {
+                Name = "workout-nest",
+                ApiSecrets = { new Secret("secret".Sha256()) },
+                UserClaims = {
+                    JwtClaimTypes.Email,
+                    JwtClaimTypes.GivenName,
+                    JwtClaimTypes.FamilyName,
+                    Id
+                },
+                Description = "Backend services od WorkoutNest",
+                DisplayName = "WorkoutNest API",
+                Enabled = true,
+                Scopes = { "workout-nest.user" },
+            }
+        };
+    }
 }
--- src/WorkoutNest.IDP/Program.cs
+++ src/WorkoutNest.IDP/Program.cs
@@ -3,14 +3,18 @@ using WorkoutNest.IDP;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddIdentityServer(options =>
-{
-    options.Events.RaiseErrorEvents = true;
-    options.Events.RaiseFailureEvents = true;
-    options.Events.RaiseInformationEvents = true;
-    options.Events.RaiseSuccessEvents = true;
-})
+    {
+        options.Events.RaiseErrorEvents = true;
+        options.Events.RaiseFailureEvents = true;
+        options.Events.RaiseInformationEvents = true;
+        options.Events.RaiseSuccessEvents = true;
+  
+    })
+    .AddTestUsers(Config.Users)
+    .AddInMemoryApiResources(Config.GetApiResources())
     .AddInMemoryApiScopes(Config.ApiScopes)
-    .AddInMemoryClients(Config.Clients);;
+    .AddInMemoryClients(Config.Clients);
+
 
 
 var app = builder.Build();
